edde0fb6a23ee2495c450eb1d3464c9af44d64ea
Atwinenickson <atwiinenicxon@gmail.com>
2022-05-19 21:06:43 +0300

    add services and images

--- src/pages/executive.js
+++ src/pages/executive.js
@@ -1,18 +1,47 @@
 import React from "react"
 import ImageGallery from 'react-image-gallery';
 
+
 const images = [
   {
     original: 'https://picsum.photos/id/1018/1000/600/',
     thumbnail: 'https://picsum.photos/id/1018/250/150/',
+    description: 'Director',
   },
   {
     original: 'https://picsum.photos/id/1015/1000/600/',
     thumbnail: 'https://picsum.photos/id/1015/250/150/',
+    description: 'Director',
   },
   {
     original: 'https://picsum.photos/id/1019/1000/600/',
     thumbnail: 'https://picsum.photos/id/1019/250/150/',
+    description: 'Director',
+  },
+  {
+    original: 'https://picsum.photos/id/1019/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1019/250/150/',
+    description: 'Director',
+  },
+  {
+    original: 'https://picsum.photos/id/1019/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1019/250/150/',
+    description: 'Director',
+  },
+  {
+    original: 'https://picsum.photos/id/1019/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1019/250/150/',
+    description: 'Director',
+  },
+  {
+    original: 'https://picsum.photos/id/1019/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1019/250/150/',
+    description: 'Director',
+  },
+  {
+    original: 'https://picsum.photos/id/1019/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1019/250/150/',
+    description: 'Director',
   },
 ];
 
